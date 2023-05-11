3a9baaf648529f34667f77277724cf3c1d3a3b47
Gian Maurício Fritsche <gianmauriciofritsche@MacBook-Air-de-Gian.local>
2023-05-11 08:09:49 -0300

    rancheiros mc

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.webp';
-import './App.css';
+import { MantineProvider, Title } from '@mantine/core';
+import { theme } from "./theme";
+import { CustomFonts } from './fonts/CustomFonts';
 
-function App() {
+export default function App() {
+  document.title = "RANCHEIROS MC";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ ...theme}}>
+      <CustomFonts/>
+      <Title color='brand'>RANCHEIROS MC</Title>
+    </MantineProvider>
   );
 }
-
-export default App;
---
+++ src/theme.ts
@@ -0,0 +1,24 @@
+import { MantineThemeOverride } from "@mantine/core";
+
+export const theme: MantineThemeOverride = {
+  colorScheme: "dark",
+  colors: {
+    brand: [
+      "#D8CAA3",
+      "#D7C082",
+      "#DCBA5E",
+      "#E8B834",
+      "#FFBB04",
+      "#D29F17",
+      "#AC8826",
+      "#8F752E",
+      "#786532",
+      "#665833"
+    ],
+  },
+  primaryColor: 'brand',
+  primaryShade: 4,
+  headings: {
+    fontFamily: 'Nandaka Western, fantasy',
+  },
+};
