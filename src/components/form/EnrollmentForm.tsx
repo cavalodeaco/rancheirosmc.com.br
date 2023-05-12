c4e2d63bcf0d615f1394b23c265ee0b93d1073f7
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-05-12 06:39:12 -0300

    fix: URL

--- src/components/form/EnrollmentForm.tsx
+++ src/components/form/EnrollmentForm.tsx
@@ -166,9 +166,8 @@ export default function EnrollmentForm(): ReactElement {
         body: data,
       };
       try {
-        console.log(process.env.REACT_APP_BACKEND_ADDRESS);
         const response = await fetch(
-          process.env.REACT_APP_BACKEND_ADDRESS as string,
+          process.env.REACT_APP_BACKEND_ADDRESS+"/enroll" as string,
           config
         );
         const { message } = await response.json();
