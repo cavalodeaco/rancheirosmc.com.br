c490195439c4cbd0316e6df24f1397f7893771e4
Gian Fritsche <gian.fritsche@gmail.com>
2023-06-08 16:13:31 -0300

    fix: resonsividade do banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,25 +1,26 @@
-import { Center, createStyles, MediaQuery, Title } from "@mantine/core";
+import {
+  Center,
+  createStyles,
+  Grid,
+  Image,
+  MediaQuery,
+  Text,
+  Title,
+} from "@mantine/core";
 import { ReactElement } from "react";
 import banner from "./img/background.jpeg";
+import rancheiro from "./img/rancheiro.webp";
 
 const useStyles = createStyles((theme) => ({
-  banner: { 
-    minHeight: "calc(100vh - 60px)",
-    [`@media (max-width: 700px)`]: {
-      minHeight: '0',
-      aspectRatio: "1 / 1",
-    },
+  banner: {
+    minHeight: "calc(min(100vh, 100vw) - 60px)",
     backgroundImage: `url(${banner})`,
     backgroundSize: "cover",
     backgroundPosition: "50% 100%",
     display: "flex",
-    filter: "sepia(1)",
   },
-  blackground: {
-    backgroundColor: "#0000008c",
+  grid: {
     width: "100%",
-    textAlign: "center",
-    alignSelf: "center",
   },
 }));
 
@@ -28,18 +29,28 @@ export default function Banner(): ReactElement {
 
   return (
     <div className={classes.banner}>
-      <div className={classes.blackground}>
-        <MediaQuery
-          query="(min-width: 700px)"
-          styles={{
-            fontSize: "5rem",
-          }}
-        >
-          <Title color={"gray.2"} size={"2.5rem"}>
+      <Grid align="center" gutter={"xs"} pt={"calc(60px + 1rem)"} className={classes.grid}>
+        <Grid.Col md={6}>
+          <Center>
+            <Image
+              src={rancheiro}
+              alt={"RANCHEIROS MC"}
+              width={"min(50vw, 50vh)"}
+              height={"min(50vw, 50vh)"}
+              maw={"50vw"}
+              mah={"50vh"}
+              fit="contain"
+            >
+              RANCHEIROS MC
+            </Image>
+          </Center>
+        </Grid.Col>
+        <Grid.Col md={6}>
+          <Title color={"gray.2"} size={"5vw"} align="center">
             RANCHEIROS MC
           </Title>
-        </MediaQuery>
-      </div>
+        </Grid.Col>
+      </Grid>
     </div>
   );
 }
