d20e3ceb7c567624b0c6e2b888a57a34ff6bc60a
Gian Fritsche <gian.fritsche@gmail.com>
2023-09-12 20:54:57 -0300

    feat(schedule): update 2023 schedule

--- src/data/schedule.js
+++ src/data/schedule.js
@@ -6,9 +6,14 @@ const schedule = [
   { date: { day: "15", month: "JULHO" }, description: "Manobras para Vida em Cambira/Jandaia do Sul" },
   { date: { day: "12", month: "AGOSTO" }, description: "Manobras para Vida em Londrina" },
   { date: { day: "02", month: "SETEMBRO" }, description: "Manobras para Vida em Maringá" },
+  { date: { day: "23", month: "SETEMBRO" }, description: "Encerramento semana Nacional do Trânsito - Mandaguari" },
+  { date: { day: "24", month: "SETEMBRO" }, description: "Culto em Faxinal-PR" },
   { date: { day: "30", month: "SETEMBRO" }, description: "Manobras para Vida em Cambira/Jandaia do Sul" },
+  { date: { day: "12", month: "OUTUBRO" }, description: "Evento dia das Crianças - Igreja Metodista em Cambira" },
   { date: { day: "21", month: "OUTUBRO" }, description: "Manobras para Vida em Londrina" },
+  { date: { day: "28", month: "OUTUBRO" }, description: "Comitiva para Guarapuava" },
   { date: { day: "11", month: "NOVEMBRO" }, description: "Manobras para Vida em Maringá" },
+  { date: { day: "03", month: "DEZEMBRO" }, description: "Confraternização de final de ano" },
 ];
 
 export default schedule;
