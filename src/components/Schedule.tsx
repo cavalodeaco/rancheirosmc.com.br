6b1f19ce1101a948037b245d27ad269b0d2014ab
Gian Fritsche <gian.fritsche@gmail.com>
2023-12-28 22:04:52 -0300

    feat(schedule): 2024 schedule

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,36 +1,42 @@
-import { Box, Flex, Table, Title } from "@mantine/core";
+import { Box, Flex, ScrollArea, Table, Title } from "@mantine/core";
 import schedule from "../data/schedule";
 
 export default function Schedule() {
   return (
     <Box py={"calc(60px + 2rem)"}>
-      <Title order={2}>Agenda 2023</Title>
+      <Title order={2}>Agenda 2024</Title>
 
-      <Table mt={"xl"}>
-        <tbody>
-          {schedule.map((event, index) => (
-            <tr key={index}>
-              <td>
-                <Flex direction={"column"} justify={"center"} align={"center"}>
-                  <Box
-                    fz={32}
-                    sx={(theme) => ({
-                      lineHeight: "40px",
-                      color: theme.colors.brand[6],
-                    })}
+      <ScrollArea h={400} type="always" offsetScrollbars scrollbarSize={18} scrollHideDelay={0}>
+        <Table mt={"xl"}>
+          <tbody>
+            {schedule.map((event, index) => (
+              <tr key={index}>
+                <td>
+                  <Flex
+                    direction={"column"}
+                    justify={"center"}
+                    align={"center"}
                   >
-                    {event.date.day}
-                  </Box>
-                  <Box fz={16}>{event.date.month}</Box>
-                </Flex>
-              </td>
-              <td>
-                <Box fz={16}>{event.description}</Box>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+                    <Box
+                      fz={32}
+                      sx={(theme) => ({
+                        lineHeight: "40px",
+                        color: theme.colors.brand[6],
+                      })}
+                    >
+                      {event.date.day}
+                    </Box>
+                    <Box fz={16}>{event.date.month}</Box>
+                  </Flex>
+                </td>
+                <td>
+                  <Box fz={16}>{event.description}</Box>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </ScrollArea>
     </Box>
   );
 }
