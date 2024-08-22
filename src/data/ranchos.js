1193a3258185d66746c93c81bb22ee81e09d9836
Gian Fritsche <gianfritsche@MacBook-Air-de-Gian.local>
2024-08-22 17:59:16 -0300

    feat: update ranchos

--- src/data/ranchos.js
+++ src/data/ranchos.js
@@ -1,81 +1,120 @@
-
-const ranchos = [{
-  name: "Cambira",
-  prosa: { day: "Toda segunda", time: "20h", address: "Av. Itália, 175" },
-  contact: {
-    tel: "+5541988435201",
-    label: "(41) 98843-5201",
-    name: "Rogério",
-    photo: "https://d2er8q8v25uk9z.cloudfront.net/pinduca.webp",
-  }
-}, {
-  name: "Maringá",
-  prosa: { day: "Toda terça", time: "20h", address: "Rua Evaristo da Veiga, 143" },
-  contact: {
-    tel: "+5541998048668",
-    label: "(41) 99804-8668",
-    name: "Roberto",
-    photo: "https://d2er8q8v25uk9z.cloudfront.net/robo.webp",
-  }
-}, {
-  name: "Londrina",
-  prosa: { day: "Toda terceira terça-feira do mês", time: "20h", address: "Rua Deputado Nilson Ribas, 551" },
-  contact: {
-    tel: "+5541999175424",
-    label: "(41) 99917-5424",
-    name: "Marcelo",
-    photo: "https://d2er8q8v25uk9z.cloudfront.net/rapa.webp",
-  }
-}, {
-  name: "Faxinal",
-  contact: {
-    tel: "+5543996117505",
-    label: "(43) 99611-7505",
-    name: "Pr. Vital",
-    photo: "https://d2er8q8v25uk9z.cloudfront.net/vital.webp",
-  }
-}, {
-  name: "Medianeira",
-  contact: {
-    tel: "+5545991078235",
-    label: "(45) 99107-8235",
-    name: "Dudu",
-    photo: "https://d2er8q8v25uk9z.cloudfront.net/dudu.webp",
-  }
-}, {
-  name: "São José dos Pinhais",
-  contact: {
-    tel: "+5541991170055",
-    label: "(41) 99117-0055",
-    name: "Daniel",
-    photo: "https://d2er8q8v25uk9z.cloudfront.net/daniel.webp",
-  }
-}, {
-  name: "Cornélio Procópio",
-  contact: {
-    tel: "+5543999127881",
-    label: ["(43) 99912-7881", "(43) 98851-8880"],
-    name: "Gabi e Douglas",
-    photo: "https://d2er8q8v25uk9z.cloudfront.net/douglas_gabi.webp",
+const ranchos = [
+  {
+    name: "Cambira-PR",
+    prosa: { day: "Toda segunda", time: "20h", address: "Av. Itália, 175" },
+    contact: {
+      tel: "+5541988435201",
+      label: "(41) 98843-5201",
+      name: "Diretor – Rogério (Pinduca)",
+      photo: "https://d2er8q8v25uk9z.cloudfront.net/pinduca.webp",
+    },
   },
-  prosa: { day: "Toda sexta", time: "19h30", address: "Rua Espírito Santo, 255" },
-}, {
-  name: "Nampula (Moçambique)",
-  contact: {
-    tel: "+5544998081276",
-    label: "(44) 99808-1276",
-    name: "Pr. Tiago",
-    photo: "https://d2er8q8v25uk9z.cloudfront.net/tiago.webp",
-  }
-}, {
-  name: "Arapongas",
-  contact: {
-    tel: "+5543991008645",
-    label: "(43) 99100-8645",
-    name: "Bia",
-    photo: "https://d2er8q8v25uk9z.cloudfront.net/bia.webp",
+  {
+    name: "Maringá-PR",
+    prosa: {
+      day: "Toda terça",
+      time: "20h",
+      address: "Rua Evaristo da Veiga, 143",
+    },
+    contact: {
+      tel: "+5541998048668",
+      label: "(41) 99804-8668",
+      name: "Representante – Roberto (Robocop)",
+      photo: "https://d2er8q8v25uk9z.cloudfront.net/robo.webp",
+    },
   },
-  prosa: { day: "Toda segunda (a partir de 8 de abril)", time: "20h", address: "Rua Pica Pau, 1833 (Espaço P Priorize-se)" },
-}];
+  {
+    name: "Londrina-PR",
+    prosa: {
+      day: "Toda terceira terça-feira do mês",
+      time: "20h",
+      address: "Rua Deputado Nilson Ribas, 551",
+    },
+    contact: {
+      tel: "+5541999175424",
+      label: "(41) 99917-5424",
+      name: "Representante – Marcelo (Rapa)",
+      photo: "https://d2er8q8v25uk9z.cloudfront.net/rapa.webp",
+    },
+  },
+  {
+    name: "Cornélio Procópio-PR",
+    contact: {
+      tel: "+5543999127881",
+      label: "(43) 99912-7881",
+      name: "Representante – Gabi",
+      photo: "https://d2er8q8v25uk9z.cloudfront.net/douglas_gabi.webp",
+    },
+    prosa: {
+      day: "Toda sexta",
+      time: "19h30",
+      address: "Rua Espírito Santo, 255",
+    },
+  },
+  {
+    name: "Arapongas-PR",
+    contact: {
+      tel: "+5543999039452",
+      label: "(43) 99903-9452",
+      name: "Representante – Júnior",
+      photo: "",
+    },
+    prosa: {
+      day: "Toda segunda",
+      time: "20h",
+      address: "",
+    },
+  },
+  {
+    name: "Medianeira-PR",
+    contact: {
+      tel: "+5545999651117",
+      label: "(45) 99965-1117",
+      name: "Diretor – Pr Júnior",
+      photo: "",
+    },
+    prosa: {
+      day: "Toda terça",
+      time: "20h",
+      address: "",
+    },
+  },
+  {
+    name: "Faxinal-PR",
+    contact: {
+      tel: "+5543999792559",
+      label: "(43) 99979-2559",
+      name: "Representante – C. Tiago",
+      photo: "",
+    },
+  },
+  {
+    name: "São José dos Pinhais - PR",
+    contact: {
+      tel: "+5541991170055",
+      label: "(41) 99117-0055",
+      name: "Representante – Pr Daniel",
+      photo: "https://d2er8q8v25uk9z.cloudfront.net/daniel.webp",
+    },
+  },
+  {
+    name: "Nampula (Moçambique)",
+    contact: {
+      tel: "+5544998081276",
+      label: "(44) 99808-1276",
+      name: "Representante – Tiago Bastos",
+      photo: "https://d2er8q8v25uk9z.cloudfront.net/tiago.webp",
+    },
+  },
+  {
+    name: "Guarulhos-SP",
+    contact: {
+      tel: "+5511915503938",
+      label: "(11) 91550-3938",
+      name: "Representante – Bia",
+      photo: "https://d2er8q8v25uk9z.cloudfront.net/bia.webp",
+    },
+  },
+];
 
 export default ranchos;
