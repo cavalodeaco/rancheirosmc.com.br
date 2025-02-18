7e27a10e14e7de238f06effd729de8c7e2432683
Gian Fritsche <gian.fritsche@gmail.com>
2025-02-17 21:17:29 -0300

    ranchos

--- src/components/Ranchos.tsx
+++ src/components/Ranchos.tsx
@@ -4,19 +4,12 @@ import {
   Box,
   Card,
   Flex,
-  Grid,
   Stack,
   Text,
   ThemeIcon,
   Title,
 } from "@mantine/core";
-import {
-  IconBadge,
-  IconBadge3d,
-  IconId,
-  IconMessages,
-  IconUser,
-} from "@tabler/icons-react";
+import { IconMessages, IconUser } from "@tabler/icons-react";
 import RancheirosText from "./RancheirosText";
 import ranchos from "../data/ranchos";
 
@@ -40,21 +33,42 @@ export default function Ranchos() {
       <Flex mt={"xl"} gap={".5rem"} justify={"center"} wrap={"wrap"}>
         {ranchos.map(({ name, prosa, contact }) => (
           <Card key={name} withBorder sx={{ width: "300px" }}>
-            <Title align="center" order={3}>
+            <Title align="center" order={4}>
               {name}
             </Title>
-            <Flex align={"center"} gap={"1rem"} mt={"xl"}>
+            <Flex
+              align={"center"}
+              gap={"0"}
+              mt={"xl"}
+              style={{ marginRight: "-1rem"}}
+            >
               <Avatar
                 sx={{ filter: "sepia(1)" }}
                 src={contact.photo}
-                radius="xl"
-                size="lg"
+                radius="100%"
+                size="xl"
                 alt={contact.name}
                 color="brand.4"
+                style={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
               >
-                <IconUser size={"2rem"} />
+                <div
+                  style={{
+                    borderTopRightRadius: 0,
+                    borderBottomRightRadius: 0,
+                  }}
+                >
+                  <IconUser size={"2rem"} />
+                </div>
               </Avatar>
-              <Flex direction={"column"}>
+              <Flex
+                direction={"column"}
+                style={{
+                  backgroundColor: "#1A1C1E",
+                  height: "84px",
+                  width: "100%",
+                  padding: "0.5rem 1rem",
+                }}
+              >
                 <Anchor href={`tel:${contact.tel}`}>
                   <ContactLabel label={contact.label} />
                 </Anchor>
@@ -63,8 +77,13 @@ export default function Ranchos() {
             </Flex>
             {prosa ? (
               <Flex align={"center"} gap={"1rem"} mt={"xl"}>
-                <ThemeIcon variant="outline" radius="xl" size={56}>
-                  <IconMessages size={32} />
+                <ThemeIcon
+                  variant="light"
+                  radius="xl"
+                  size={36}
+                  style={{ borderBottomLeftRadius: 0 }}
+                >
+                  <IconMessages size={24} />
                 </ThemeIcon>
                 <Flex direction={"column"}>
                   <RancheirosText highlight={["Prosa"]} mt={0}>
